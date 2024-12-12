b400b5b35b4d6064764563cf325b6b16cacbf048
Enmyr <natebarratt06@gmail.com>
2024-12-12 16:49:07 -0500

    Game decided. Added most assets from original.

--- scripts/time_elapsed.cs
+++ scripts/time_elapsed.cs
@@ -5,12 +5,12 @@ public partial class time_elapsed : Label
 {
 	// Called when the node enters the scene tree for the first time.
 	private float time = 0.0f;
-	private Label LabelTime;
+	private Label TimeLabel;
 	public override void _Ready()
 	{
 		base._Ready();
 
-		LabelTime = GetNode<Label>("LabelTime");
+		TimeLabel = GetNode<Label>("LabelTime");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,6 +20,6 @@ public partial class time_elapsed : Label
 
 		time += (float)delta;
 
-		LabelTime.Text = $"Time elapsed since start: {time:F1} s";
+		TimeLabel.Text = $"Time elapsed since start: {time:F1} s";
 	}
 }
